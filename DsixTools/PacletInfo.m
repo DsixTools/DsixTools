1a93841c7484894e34880f2d94971a74632a54a1
Avelino Vicente <avelino.vicente@ific.uv.es>
2020-10-29 16:09:28 +0100

    DsixTools v2.0

---
+++ DsixTools/PacletInfo.m
@@ -0,0 +1,17 @@
+Paclet[
+	Name -> "DsixTools",
+	Version -> "2.0",
+	MathematicaVersion -> "9.0+",
+	Description -> "DsixTools is a Mathematica Package for the matching and renormalization-group evolution from the new-physics scale to the scale of low-energy observables.",
+	Creator -> "Alejandro Celis, Javier Fuentes-Martin, Pedro Ruiz-Femenia, Avelino Vicente, Javier Virto",
+	URL -> "https://dsixtools.github.io/",
+	Thumbnail -> "logo.png",
+	Extensions ->{
+		{
+		"Documentation",
+		Language -> "English",
+		LinkBase -> "DsixTools",
+		MainPage -> "Guides/DsixTools"
+		}
+	}
+]
